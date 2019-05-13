27a985eca1ffc34c7c8758ce6febf6023a3caec7
Michele Pugno <doc5.129@gmail.com>
2019-05-13 06:49:58 +0200

    new main in shooter

---
+++ shooter.py
@@ -0,0 +1,27 @@
+"""Shooter
+
+Usage:
+    shooter.py 
+"""
+
+from game_mode import *
+import docopt
+
+if __name__ == "__main__":
+    # Initialize pygame
+    pygame.init()
+    args = sys.argv
+    if '-d' in args:
+        debug = True
+    else:
+        debug = False
+    if 'test' in args:
+        GAME = TestGame((2500, 2500), (1300, 800), debug) # mapsize, screensize, debug_enable
+        GAME.run()
+    if '1v1client' in args:
+        ip = args[2]
+        GAME = Multi1v1((2500, 2500), (1300, 800), 'client', debug, ip) # mapsize, screensize, debug_enable
+        GAME.run()
+    if '1v1server' in args:
+        GAME = Multi1v1((2500, 2500), (1300, 800), 'server', debug) # mapsize, screensize, debug_enable
+        GAME.run()
